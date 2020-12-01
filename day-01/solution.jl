e76e3956922865b802a002db41c1eae7162618ce
Damien Gonot <damien.gonot@gmail.com>
2020-12-01 10:31:26 -0500

    day 1 part 2 solution in Julia

--- day-01/solution.jl
+++ day-01/solution.jl
@@ -1,8 +1,19 @@
 input = open(f->read(f, String), "input") |> f->split(f, "\n") |> a->map(s->parse(Int, s), a)
 
+# Part 1
 for i ∈ input, j ∈ input
     if i+j == 2020
+        println("Part 1:")
         println(i*j)
+        break
+    end
+end
+
+# Part 2
+for i ∈ input, j ∈ input, k ∈ input
+    if i+j+k == 2020
+        println("Part 2:")
+        println(i*j*k)
         break
     end
 end
