72a05c285cb3daeb44a221a679820c3417f686a5
Damien Gonot <damien.gonot@gmail.com>
2020-12-13 10:28:26 -0500

    day 7 part 2 solution in Julia

--- day-07/solution.jl
+++ day-07/solution.jl
@@ -27,4 +27,30 @@ while count < colour_names |> unique |> length
     end
 end
 
+println("Part 1:")
 println(count)
+
+global directory_with_numbers = Dict()
+for line in input
+    contain_split = split(line, " bags contain ")
+    colour, rest = contain_split[1], strip(contain_split[2], '.')
+    contain = split(rest, ", ") |> s->map(a->(split(a)[1] == "no" ? 0 : parse(Int, split(a)[1]), join([split(a)[2], split(a)[3]], " ")), s)
+    directory_with_numbers[colour] = contain
+end
+
+function how_many_bags(bag_colour)
+    if bag_colour == "other bags"
+        return 0
+    end
+    list_of_bags = directory_with_numbers[bag_colour]
+    count = 1
+    for bag_pair in list_of_bags
+        n, colour = bag_pair
+        count += n*how_many_bags(colour)
+    end
+    return count
+end
+
+println("Part 2:")
+# We include the bag itself in the count of how_many_bags(), therefore we have to substract 1 as we shouldn't count the shiny gold bag itself
+println(how_many_bags("shiny gold")-1)
