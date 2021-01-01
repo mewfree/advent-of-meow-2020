b37e15a516bc3330fe588f5697640a97ec4cc2a8
Damien Gonot <damien.gonot@gmail.com>
2021-01-01 00:59:22 -0500

    day 9 part 2 solution in Julia

--- day-09/solution.jl
+++ day-09/solution.jl
@@ -7,8 +7,20 @@ for (index, value) in enumerate(input)
         preamble = input[index-preamble_size:index-1]
         pairs = [(a, b) for a in preamble, b in preamble if a != b]
         if value ∉ sum.(pairs)
-            println(value)
+            global result = value
+            println("Part 1:")
+            println(result)
             break
         end
+    end
+end
+
+set_ranges = [i:j for i in 1:length(input), j in 1:length(input) if j > i]
+for set_range in set_ranges
+    set = input[set_range]
+    if set |> sum == result
+        println("Part 2:")
+        println(minimum(set) + maximum(set))
+        break
     end
 end
